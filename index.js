97e33a479225097a4b9c41873e104d82ffe3f230
Harsh Kumar <90533930+kumarharshh@users.noreply.github.com>
2023-12-04 21:20:59 +0530

    Button Created

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", ()=>{
+    document.getElementById("btn").style.backgroundColor='black';
+    document.getElementById("btn").innerHTML="Saved";
+})
