0ff4418312e1c288d94b3a4531f117613d8a3325
Harsh Kumar <90533930+kumarharshh@users.noreply.github.com>
2023-12-05 21:37:35 +0530

    Add files via upload

---
+++ three/dd.js
@@ -0,0 +1,8 @@
+document.getElementById("opt").addEventListener("click", ()=>{
+    var x=document.getElementById("choose");
+    if(x.style.display === "block"){
+        x.style.display="none";
+    }else{
+        x.style.display="block";
+    }
+})
