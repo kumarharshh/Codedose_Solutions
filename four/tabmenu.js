58dd5c16e23c88e2ec54f1b57c06643417d8190b
Harsh Kumar <90533930+kumarharshh@users.noreply.github.com>
2023-12-06 01:13:28 +0530

    Tab Menu Created
    This is a tab menu when you click on the button the content changes for the div. Just how a nav bar works.

---
+++ four/tabmenu.js
@@ -0,0 +1,30 @@
+document.getElementById("p").addEventListener("click",()=>{
+    document.getElementById("conn").innerHTML="Drinking Coffee with whey protien and it's benefits"
+    document.getElementById("time").innerHTML="6h ago"
+    document.getElementById("comm").innerHTML="8 comments"
+    document.getElementById("likes").innerHTML="3 likes"
+    document.getElementById("conn1").innerHTML="Is tech making coffer better or worse?"
+    document.getElementById("time1").innerHTML="8h ago"
+    document.getElementById("comm1").innerHTML="2 comments"
+    document.getElementById("likes1").innerHTML="4 likes"
+})
+document.getElementById("t").addEventListener("click",()=>{
+    document.getElementById("conn").innerHTML="Ask me anything: about making coffee"
+    document.getElementById("time").innerHTML="3d ago"
+    document.getElementById("comm").innerHTML="81 comments"
+    document.getElementById("likes").innerHTML="32 likes"
+    document.getElementById("conn1").innerHTML="The worst advice you've ever heard about coffee"
+    document.getElementById("time1").innerHTML="21h ago"
+    document.getElementById("comm1").innerHTML="22 comments"
+    document.getElementById("likes1").innerHTML="12 likes"
+})
+document.getElementById("r").addEventListener("click",()=>{
+    document.getElementById("conn").innerHTML="Drinking Coffee makes you stronger"
+    document.getElementById("time").innerHTML="5h ago"
+    document.getElementById("comm").innerHTML="5 comments"
+    document.getElementById("likes").innerHTML="5 likes"
+    document.getElementById("conn1").innerHTML="So you've bought coffee what now?"
+    document.getElementById("time1").innerHTML="6h ago"
+    document.getElementById("comm1").innerHTML="4 comments"
+    document.getElementById("likes1").innerHTML="2 likes"
+})
